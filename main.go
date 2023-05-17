5fb0c1ecc9bc71e21294622bee49b7dc9de27f77
liut <liutao@hyyl.xyz>
2023-05-17 11:18:42 +0800

    arguments as a config

--- main.go
+++ main.go
@@ -23,42 +23,47 @@ func init() {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
 }
+
+type Config struct {
+	port int
+	ttl  uint
+	days uint
+	serv bool
+	net  string
+	dsn  string
+	name string
+	ip   string
+}
+
 func main() {
-	var (
-		port int
-		dsn  string
-		name string
-		ip   string
-		ttl  uint
-		days uint
-		serv bool
-	)
-	flag.IntVar(&port, "port", 1353, "listen port")
-	flag.StringVar(&dsn, "dsn", envOr("CICADA_REDIS_DSN", "redis://localhost:6379/0"), "redis connection string")
-	flag.StringVar(&name, "name", "", "host for add")
-	flag.StringVar(&ip, "ip", "", "ip for add")
-	flag.UintVar(&ttl, "ttl", 60, "time to live of renew cache")
-	flag.UintVar(&days, "days", dftDays, "expire in some days")
-	flag.BoolVar(&serv, "serv", false, "run as dns server")
+	var cfg Config
+	flag.IntVar(&cfg.port, "port", 1353, "listen port")
+	flag.StringVar(&cfg.dsn, "dsn", envOr("CICADA_REDIS_DSN", "redis://localhost:6379/0"), "redis connection string")
+	flag.StringVar(&cfg.name, "name", "", "host for add")
+	flag.StringVar(&cfg.ip, "ip", "", "ip for add")
+	flag.UintVar(&cfg.ttl, "ttl", 60, "time to live of renew cache")
+	flag.UintVar(&cfg.days, "days", dftDays, "expire in some days")
+	flag.BoolVar(&cfg.serv, "serv", false, "run as dns server")
+	flag.StringVar(&cfg.net, "net", "udp", "network: udp|tcp")
 	flag.Parse()
 
-	log.Info().Str("ver", version).Int("port", port).Msg("starting")
-	if serv {
-		srv := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
-		srv.Handler = NewMux(dsn)
+	log.Info().Str("ver", version).Int("port", cfg.port).Msg("starting")
+	if cfg.serv {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(cfg.port), Net: "udp"}
+		srv.Handler = NewMux(cfg.dsn)
 		if err := srv.ListenAndServe(); err != nil {
 			log.Fatal().Err(err).Msg("fail to udp listen")
 		}
-	} else if len(name) == 0 || len(ip) == 0 {
+	} else if len(cfg.name) == 0 || len(cfg.ip) == 0 {
 		flag.Usage()
 	} else {
-		h := NewMux(dsn)
-		expiration := time.Hour * 24 * time.Duration(days)
-		err := h.Set(NewA(name, ip, ttl), expiration)
+		h := NewMux(cfg.dsn)
+		expiration := time.Hour * 24 * time.Duration(cfg.days)
+		err := h.Set(NewA(cfg.name, cfg.ip, cfg.ttl), expiration)
 		if err != nil {
 			log.Error().Err(err).Msg("add record fail")
 		} else {
-			log.Info().Str("name", name).Str("ip", ip).Msg("add record ok")
+			log.Info().Str("name", cfg.name).Str("ip", cfg.ip).Msg("add record ok")
 		}
 	}
 }
